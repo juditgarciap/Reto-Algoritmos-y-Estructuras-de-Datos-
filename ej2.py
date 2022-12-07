7e9407bced1a7489c2e308f0019cace18a4ed0b0
juditgarciap <judit.garciapadronleon@usp.ceu.es>
2022-12-07 10:37:06 +0000

    ej n reinas

--- ej2.py
+++ ej2.py
@@ -1,1 +1,18 @@
 #n reinas back tracking
+# numero de reinas
+n = 6
+# contador controla si encontro una solucion
+contador = 0
+columna = [False]*n
+# las diagonales ayudan para saber si las reinas
+# comparten diagonales por lo tanto se amenazan
+diagonal_izquierda = [False]*(n*2)
+diagonal_derecha = [False]*(n*2)
+solucion = []
+
+
+# función recursiva
+def backtrack(y,n,contador):
+    if (y==n):
+        #retorna
+        return contador + 1
